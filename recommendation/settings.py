e7b3f7bbeb9f129dab8f8fc521e90cc0506fd563
SanjibKarki <sanjeebkarki64@gmail.com>
2026-02-18 14:13:33 +0545

    kubernetesconfig

--- recommendation/settings.py
+++ recommendation/settings.py
@@ -28,14 +28,7 @@ DEBUG = False
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = [
-    '192.168.1.7',
-    'localhost',
-    '127.0.0.1',
-    'web',
-    'e-commerce-website-web-1',   # Gunicorn container name
-    'e-commerce-website-nginx-1', # Nginx container name
-]
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = "Accounts.User"
 LOGIN_URL = '/login'
 # Application definition
